eb327270cf348abc4bb155a961cf03a5cc19e3e2
Jiewen Yao <jiewen.yao@intel.com>
2018-09-27 15:37:15 +0800

    Revert "Add BldNetworkFv.bat and NetworkPkg.fdf to generate network binary FV."
    This reverts commit b7f69a3e49e63ecac73a21c5ca161ab999934cbc.
    
    Add network FV build in MinPlatformPkg, along with other Core FV.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>

--- BldNetworkFv.bat
+++
@@ -1,4 +0,0 @@
-call build -a X64 -t VS2015x86 -p NetworkPkg\NetworkPkg.dsc -f NetworkPkg\NetworkPkg.fdf
-call build -a X64 -t VS2015x86 -p NetworkPkg\NetworkPkg.dsc -f NetworkPkg\NetworkPkg.fdf -b RELEASE
-copy Build\NetworkPkg\DEBUG_VS2015x86\FV\FVUEFINETWORK.Fv CoreFvBin\Network\DEBUG\FVUEFINETWORK.Fv /y
-copy Build\NetworkPkg\RELEASE_VS2015x86\FV\FVUEFINETWORK.Fv CoreFvBin\Network\RELEASE\FVUEFINETWORK.Fv /y
